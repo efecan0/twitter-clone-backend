08463ecefbf3af6ffeb36fd2722cfd3ace4fa400
efecan <efecanerdem0907@gmail.com>
2022-02-04 18:19:51 +0300

    user models completed, userRouter is under construction

---
+++ models/user.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    name: {
+        type: String,
+        default: '',
+        require: true
+    },
+    email:{
+        type: String,
+        default: ''
+    },
+    profilePicture:{
+        type: String,
+        default: ''
+    },
+    bio:{
+        type: String,
+        default: ''
+    },
+    location:{
+        type: String,
+        default: ''
+    },
+    website:{
+        type: String,
+        default: ''
+    },
+    admin:{
+        type: Boolean,
+        default: false
+    }
+},{
+    timestamps: true
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+router.use(bodyParser.json());
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin ,(req, res, next) => {
+  User.find({})
+  .then((users) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(users);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}),
+  req.body.password, (err, res) => {
+    if(err) {
+      res.statusCode = 200;
+      res.setHeader = ('Content-Type', 'application/json');
+      res.json({err: err});
+    }else{
+      if(req.body.name){
+        user.name = req.body.name;
+      }
+      if(req.body.email){
+        user.email = req.body.email
+      }     
+      user.save((err, user) => {
+        if(err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local') ,(req, res) => {
+
+})
+
 
 module.exports = router;
