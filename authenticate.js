6fd970936cbd3bca2c5458c286e340f34e0de2a9
efecan <efecanerdem0907@gmail.com>
2022-02-07 14:23:22 +0300

    authenticate config added

--- authenticate.js
+++ authenticate.js
@@ -1,12 +1,12 @@
 var passport = require('passport');
-var localStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
 
-
+var config = require('./config.js');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -23,7 +23,7 @@ opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
-        console.log("JWT payload" + jwt_payload);
+        console.log("JWT payload: " + jwt_payload);
         User.findOne({_id: jwt_payload._id}, (err, user) =>{
             if(err) {
                 return done(err, false);
