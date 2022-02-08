32a1df74d263b0b96f77a6bdf9310a1839e1626c
efecan <efecanerdem0907@gmail.com>
2022-02-08 18:30:32 +0300

    favorite model

--- models/favorite.js
+++ models/favorite.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const favoriteSchema = new Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'user'   
+        ref:'User'   
        },
        tweets: [
         {
